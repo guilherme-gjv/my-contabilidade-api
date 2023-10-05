a5c88228d9df3c4a0b159b29a4ce382fc2ea45f2
guilherme-gjv <guijesus.info@gmail.com>
2023-10-05 00:02:23 -0300

    fix: updated user repository import

--- src/app/repositories/UserRepository.ts
+++ src/app/repositories/UserRepository.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { userSelect } from "../PrismaSelects";
-import { IUser } from "../models/IUser";
+import { IUser } from "../domain/models/IUser";
 
 const prisma = new PrismaClient();
 
