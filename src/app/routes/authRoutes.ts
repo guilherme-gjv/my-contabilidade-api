2e691c79eb73d0c19474155616c10281df6137ce
guilherme-gjv <guijesus.info@gmail.com>
2023-10-09 19:58:10 -0300

    feat: /auth/me route

--- src/app/routes/authRoutes.ts
+++ src/app/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController";
+import AuthMiddleware from "../middlewares/AuthMiddleware";
 
 const router = Router();
 
 router.post("/login", AuthController.login);
+router.get("/me", AuthMiddleware.authenticate, AuthController.me);
 
 export default router;
